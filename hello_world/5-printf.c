04d818c0d89e1c322af559e6e557b87c49bc0e04
EmmanuelFarsi <emmanuel.farsi@protonmail.com>
2023-06-16 09:59:26 -0700

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printf() : display "with proper grammar, but the outcome is a piece of art,"
+ * return (0) : Your program should return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
