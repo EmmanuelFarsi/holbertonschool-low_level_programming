9d179f8e9c77280ccf252c559f85b7bf93679ff6
EmmanuelFarsi <emmanuel.farsi@protonmail.com>
2023-06-20 06:39:17 -0700

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: always (0)
+ *
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
