362bd2fc8480f1966cc32512638ffe9b89c56836
EmmanuelFarsi <emmanuel.farsi@protonmail.com>
2023-06-21 07:34:43 -0700

    2. 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+
+void print_alphabet_x10(void)
+{
+	char lowercase;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		{
+			_putchar(lowercase);
+		}
+		_putchar('\n');
+	}
+}
